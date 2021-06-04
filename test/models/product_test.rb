c898e4cffed947166f23b6ed32cbcc18406ca16a
Ignacio Alonso <ignacio.alley@gmail.com>
2021-06-04 10:16:47 -0500

    Add gh action

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "#full_name" do
+    user = users(:george)
+
+    assert_equal "George Abitbol", user.full_name
+  end
 end
